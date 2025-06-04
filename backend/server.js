2af66c66aa6963aa0a84fd504419b4f2d2b82d5b
Mihir Biswas <biswasmihir355@gmail.com>
2025-06-04 21:28:16 +0530

    new project

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
+const todoRoutes = require("./routes/todoRoutes");
+const timerRoutes = require("./routes/timerRoutes"); // ✅ add this
+const summaryRoutes = require("./routes/summaryRoutes");
 
 dotenv.config();
 
@@ -12,7 +15,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/summary", summaryRoutes);
 app.use("/api/auth", userRoutes);
+app.use("/api/todos", todoRoutes);
+app.use("/api/timer", timerRoutes); // ✅ mount timer routes
 
 mongoose
   .connect(process.env.MONGO_URI, {
